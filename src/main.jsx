c3fed6bf99c6df41dd908f7cef2ad904449ab57e
FilipSpioch <biedaczeq@gmail.com>
2025-03-02 15:27:46 +0100

    godziny - funkcja
    funkcja wywoływana w klamrach

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,9 @@ import ReactDOM from "react-dom/client";
 
 const data = new Date();
 const godzina = data.getHours();
-const minuta = data.getMinutes();
-const element = (
-  <h1>
-    {godzina} {minuta}
-  </h1>
-);
+
+const Funkcja = (hr) => <h1>{hr}</h1>;
+
+const element = <>{Funkcja(godzina)}</>;
+
 ReactDOM.createRoot(document.getElementById("root")).render(element);
